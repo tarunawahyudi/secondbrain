42a57da5ef8ecef9c4cc75c65b0345022eb185f0
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2024-10-20 16:46:00 +0700

    upload banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import {ThemeProvider} from "@/components/providers/theme-provider";
 import {ConvexClientProvider} from "@/components/providers/convex-provider";
 import {ModalProvider} from "@/components/providers/modal-provider";
+import {EdgeStoreProvider} from "@/lib/edgestore";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -45,17 +46,19 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <ConvexClientProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="scribe-theme-2"
-        >
-          <Toaster position="bottom-center" />
-          <ModalProvider />
-          {children}
-        </ThemeProvider>
+        <EdgeStoreProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="scribe-theme-2"
+          >
+            <Toaster position="bottom-center" />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </EdgeStoreProvider>
       </ConvexClientProvider>
       </body>
     </html>
