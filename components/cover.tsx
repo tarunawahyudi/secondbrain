bc2e2e31200918931f4156342f7fb02f8b1d258f
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2024-10-21 01:19:38 +0700

    editor

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import {useMutation} from "convex/react";
 import {useParams} from "next/navigation";
 import {Id} from "@/convex/_generated/dataModel";
 import {useEdgeStore} from "@/lib/edgestore";
+import {Skeleton} from "@/components/ui/skeleton";
 
 interface CoverImageProps {
   url?: string;
@@ -71,5 +72,11 @@ export const Cover = ({url, preview}: CoverImageProps) => {
         </div>
       )}
     </div>
+  )
+}
+
+Cover.Skeleton = function CoverSkeleton() {
+  return (
+    <Skeleton className="w-full h-[12vh]" />
   )
 }
---
+++ components/editor.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import "@blocknote/core/fonts/inter.css";
+import {
+  PartialBlock
+} from "@blocknote/core";
+
+import {
+  BlockNoteView
+} from "@blocknote/mantine";
+
+import {
+  useCreateBlockNote
+} from "@blocknote/react";
+
+import "@blocknote/mantine/style.css";
+import {useTheme} from "next-themes";
+import {useEffect} from "react";
+import {useEdgeStore} from "@/lib/edgestore";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+export const Editor = ({onChange, initialContent, editable}: EditorProps) => {
+
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleChange = () => {
+    const currentContent = JSON.stringify(editor.document);
+    onChange(currentContent);
+  }
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({ file });
+    return response.url;
+  }
+
+  const handleDelete = async (url: string)  => {
+    await edgestore.publicFiles.delete({ url });
+  }
+
+  const editor = useCreateBlockNote({
+    initialContent: initialContent
+      ? JSON.parse(initialContent) as PartialBlock[]
+      : undefined,
+    uploadFile: handleUpload
+  });
+
+  useEffect(() => {
+    handleChange();
+  }, [editor.document]);
+
+  return (
+    <div>
+      <BlockNoteView
+
+        onChange={handleChange}
+        editable={editable}
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
+    </div>
+  )
+}
